29c5f4e49e7779e88352d2b21e7e1d564a546c34
orxfun <orx.ugur.arikan@gmail.com>
2025-07-04 10:09:16 +0200

    MetaQueueOf is renamed as MetaQueueFrom

--- src/queue/from_sequence.rs
+++ src/queue/from_sequence.rs
@@ -1,17 +1,17 @@
 use crate::queue::{Empty, MetaQueue};
 
 /// Creates the [`MetaQueue`] with no types; i.e., returns [`Empty`].
-pub type MetaQueueOf0 = Empty;
+pub type MetaQueueFrom0 = Empty;
 
 /// Creates the [`MetaQueue`] with a single type; i.e., returns [`One`] of `T1`.
 ///
 /// [`One`]: crate::queue::One
-pub type MetaQueueOf1<T1> = <MetaQueueOf0 as MetaQueue>::Push<T1>;
+pub type MetaQueueFrom1<T1> = <MetaQueueFrom0 as MetaQueue>::Push<T1>;
 
 /// Creates the [`MetaQueue`] with two types; i.e., returns [`Multi`] of `T1` and `T2`.
 ///
 /// [`Multi`]: crate::queue::Multi
-pub type MetaQueueOf2<T1, T2> = <MetaQueueOf1<T1> as MetaQueue>::Push<T2>;
+pub type MetaQueueFrom2<T1, T2> = <MetaQueueFrom1<T1> as MetaQueue>::Push<T2>;
 
 /// Creates the [`MetaQueue`] of the given type arguments.
 ///
@@ -41,13 +41,13 @@ pub type MetaQueueOf2<T1, T2> = <MetaQueueOf1<T1> as MetaQueue>::Push<T2>;
 ///     assert_eq!(a, b);
 /// }
 ///
-/// let x = MetaQueueOf6::<char, u32, String, bool, u8, f64>::default();
+/// let x = MetaQueueFrom6::<char, u32, String, bool, u8, f64>::default();
 /// assert_type(
 ///     &x,
 ///     "Multi<char,Multi<u32,Multi<String,Multi<bool,Multi<u8,One<f64>>>>>>",
 /// );
 /// ```
-pub type MetaQueueOf3<T1, T2, T3> = <MetaQueueOf2<T1, T2> as MetaQueue>::Push<T3>;
+pub type MetaQueueFrom3<T1, T2, T3> = <MetaQueueFrom2<T1, T2> as MetaQueue>::Push<T3>;
 
 /// Creates the [`MetaQueue`] of the given type arguments.
 ///
@@ -77,13 +77,13 @@ pub type MetaQueueOf3<T1, T2, T3> = <MetaQueueOf2<T1, T2> as MetaQueue>::Push<T3
 ///     assert_eq!(a, b);
 /// }
 ///
-/// let x = MetaQueueOf6::<char, u32, String, bool, u8, f64>::default();
+/// let x = MetaQueueFrom6::<char, u32, String, bool, u8, f64>::default();
 /// assert_type(
 ///     &x,
 ///     "Multi<char,Multi<u32,Multi<String,Multi<bool,Multi<u8,One<f64>>>>>>",
 /// );
 /// ```
-pub type MetaQueueOf4<T1, T2, T3, T4> = <MetaQueueOf3<T1, T2, T3> as MetaQueue>::Push<T4>;
+pub type MetaQueueFrom4<T1, T2, T3, T4> = <MetaQueueFrom3<T1, T2, T3> as MetaQueue>::Push<T4>;
 
 /// Creates the [`MetaQueue`] of the given type arguments.
 ///
@@ -113,13 +113,14 @@ pub type MetaQueueOf4<T1, T2, T3, T4> = <MetaQueueOf3<T1, T2, T3> as MetaQueue>:
 ///     assert_eq!(a, b);
 /// }
 ///
-/// let x = MetaQueueOf6::<char, u32, String, bool, u8, f64>::default();
+/// let x = MetaQueueFrom6::<char, u32, String, bool, u8, f64>::default();
 /// assert_type(
 ///     &x,
 ///     "Multi<char,Multi<u32,Multi<String,Multi<bool,Multi<u8,One<f64>>>>>>",
 /// );
 /// ```
-pub type MetaQueueOf5<T1, T2, T3, T4, T5> = <MetaQueueOf4<T1, T2, T3, T4> as MetaQueue>::Push<T5>;
+pub type MetaQueueFrom5<T1, T2, T3, T4, T5> =
+    <MetaQueueFrom4<T1, T2, T3, T4> as MetaQueue>::Push<T5>;
 
 /// Creates the [`MetaQueue`] of the given type arguments.
 ///
@@ -149,14 +150,14 @@ pub type MetaQueueOf5<T1, T2, T3, T4, T5> = <MetaQueueOf4<T1, T2, T3, T4> as Met
 ///     assert_eq!(a, b);
 /// }
 ///
-/// let x = MetaQueueOf6::<char, u32, String, bool, u8, f64>::default();
+/// let x = MetaQueueFrom6::<char, u32, String, bool, u8, f64>::default();
 /// assert_type(
 ///     &x,
 ///     "Multi<char,Multi<u32,Multi<String,Multi<bool,Multi<u8,One<f64>>>>>>",
 /// );
 /// ```
-pub type MetaQueueOf6<T1, T2, T3, T4, T5, T6> =
-    <MetaQueueOf5<T1, T2, T3, T4, T5> as MetaQueue>::Push<T6>;
+pub type MetaQueueFrom6<T1, T2, T3, T4, T5, T6> =
+    <MetaQueueFrom5<T1, T2, T3, T4, T5> as MetaQueue>::Push<T6>;
 
 /// Creates the [`MetaQueue`] of the given type arguments.
 ///
@@ -186,14 +187,14 @@ pub type MetaQueueOf6<T1, T2, T3, T4, T5, T6> =
 ///     assert_eq!(a, b);
 /// }
 ///
-/// let x = MetaQueueOf6::<char, u32, String, bool, u8, f64>::default();
+/// let x = MetaQueueFrom6::<char, u32, String, bool, u8, f64>::default();
 /// assert_type(
 ///     &x,
 ///     "Multi<char,Multi<u32,Multi<String,Multi<bool,Multi<u8,One<f64>>>>>>",
 /// );
 /// ```
-pub type MetaQueueOf7<T1, T2, T3, T4, T5, T6, T7> =
-    <MetaQueueOf6<T1, T2, T3, T4, T5, T6> as MetaQueue>::Push<T7>;
+pub type MetaQueueFrom7<T1, T2, T3, T4, T5, T6, T7> =
+    <MetaQueueFrom6<T1, T2, T3, T4, T5, T6> as MetaQueue>::Push<T7>;
 
 /// Creates the [`MetaQueue`] of the given type arguments.
 ///
@@ -223,11 +224,11 @@ pub type MetaQueueOf7<T1, T2, T3, T4, T5, T6, T7> =
 ///     assert_eq!(a, b);
 /// }
 ///
-/// let x = MetaQueueOf6::<char, u32, String, bool, u8, f64>::default();
+/// let x = MetaQueueFrom6::<char, u32, String, bool, u8, f64>::default();
 /// assert_type(
 ///     &x,
 ///     "Multi<char,Multi<u32,Multi<String,Multi<bool,Multi<u8,One<f64>>>>>>",
 /// );
 /// ```
-pub type MetaQueueOf8<T1, T2, T3, T4, T5, T6, T7, T8> =
-    <MetaQueueOf7<T1, T2, T3, T4, T5, T6, T7> as MetaQueue>::Push<T8>;
+pub type MetaQueueFrom8<T1, T2, T3, T4, T5, T6, T7, T8> =
+    <MetaQueueFrom7<T1, T2, T3, T4, T5, T6, T7> as MetaQueue>::Push<T8>;
--- src/queue/tests/from_sequence.rs
+++ src/queue/tests/from_sequence.rs
@@ -3,41 +3,41 @@ use crate::queue::from_sequence::*;
 
 #[test]
 fn empty() {
-    let x = MetaQueueOf0::default();
+    let x = MetaQueueFrom0::default();
 
     assert_type(&x, "Empty");
 }
 
 #[test]
 fn one() {
-    let x = MetaQueueOf1::<char>::default();
+    let x = MetaQueueFrom1::<char>::default();
 
     assert_type(&x, "One<char>");
 }
 
 #[test]
 fn two() {
-    let x = MetaQueueOf2::<char, u32>::default();
+    let x = MetaQueueFrom2::<char, u32>::default();
 
     assert_type(&x, "Multi<char,One<u32>>");
 }
 
 #[test]
 fn three() {
-    let x = MetaQueueOf3::<char, u32, String>::default();
+    let x = MetaQueueFrom3::<char, u32, String>::default();
     assert_type(&x, "Multi<char,Multi<u32,One<String>>>");
 }
 
 #[test]
 fn four() {
-    let x = MetaQueueOf4::<char, u32, String, bool>::default();
+    let x = MetaQueueFrom4::<char, u32, String, bool>::default();
 
     assert_type(&x, "Multi<char,Multi<u32,Multi<String,One<bool>>>>");
 }
 
 #[test]
 fn five() {
-    let x = MetaQueueOf5::<char, u32, String, bool, u8>::default();
+    let x = MetaQueueFrom5::<char, u32, String, bool, u8>::default();
 
     assert_type(
         &x,
@@ -47,7 +47,7 @@ fn five() {
 
 #[test]
 fn six() {
-    let x = MetaQueueOf6::<char, u32, String, bool, u8, f64>::default();
+    let x = MetaQueueFrom6::<char, u32, String, bool, u8, f64>::default();
 
     assert_type(
         &x,
@@ -57,7 +57,7 @@ fn six() {
 
 #[test]
 fn seven() {
-    let x = MetaQueueOf7::<char, u32, String, bool, u8, f64, ()>::default();
+    let x = MetaQueueFrom7::<char, u32, String, bool, u8, f64, ()>::default();
 
     assert_type(
         &x,
@@ -67,7 +67,7 @@ fn seven() {
 
 #[test]
 fn eight() {
-    let x = MetaQueueOf8::<char, u32, String, bool, u8, f64, (), Vec<usize>>::default();
+    let x = MetaQueueFrom8::<char, u32, String, bool, u8, f64, (), Vec<usize>>::default();
 
     assert_type(
         &x,
