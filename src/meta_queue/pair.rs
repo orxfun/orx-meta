46a4ce5774107460c6058e0e622ad60cbf94185f
orxfun <orx.ugur.arikan@gmail.com>
2025-07-25 15:50:50 +0200

    ctors simplified

--- src/meta_queue/pair.rs
+++ src/meta_queue/pair.rs
@@ -2,15 +2,11 @@ use super::queue::MetaQueue;
 use core::marker::PhantomData;
 
 /// A meta queue containing at least two types.
-pub struct Pair<F, B> {
-    p: PhantomData<(F, B)>,
-}
+pub struct Pair<F, B>(PhantomData<(F, B)>);
 
 impl<F, B> Default for Pair<F, B> {
     fn default() -> Self {
-        Self {
-            p: Default::default(),
-        }
+        Self(PhantomData)
     }
 }
 
--- src/meta_queue/single.rs
+++ src/meta_queue/single.rs
@@ -2,15 +2,11 @@ use crate::meta_queue::{empty::Empty, pair::Pair, queue::MetaQueue};
 use core::marker::PhantomData;
 
 /// A meta queue containing exactly one type, which is the generic argument `T`.
-pub struct Single<T> {
-    p: PhantomData<T>,
-}
+pub struct Single<T>(PhantomData<T>);
 
 impl<T> Default for Single<T> {
     fn default() -> Self {
-        Self {
-            p: Default::default(),
-        }
+        Self(PhantomData)
     }
 }
 
