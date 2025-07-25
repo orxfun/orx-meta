b6661ec18112687ee5fe2d74a68142017635752f
orxfun <orx.ugur.arikan@gmail.com>
2025-07-25 16:56:24 +0200

    impl Composition for QueueComposition

--- src/queue/composition.rs
+++ src/queue/composition.rs
@@ -1,2 +1,12 @@
+use crate::{Composition, queue::EmptyQueue};
+
 #[derive(Default)]
 pub struct QueueComposition;
+
+impl Composition for QueueComposition {
+    type Empty = EmptyQueue;
+
+    fn empty() -> Self::Empty {
+        EmptyQueue
+    }
+}
