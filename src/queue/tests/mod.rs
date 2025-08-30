1912838d9dbba447bd730e554c3ac941eab5ddd7
orxfun <orx.ugur.arikan@gmail.com>
2025-08-30 19:49:06 +0200

    demonstrate nonempty queue with requirements

--- src/queue/tests/mod.rs
+++ src/queue/tests/mod.rs
@@ -1,4 +1,6 @@
 mod manual_nonempty_queue;
+mod manual_nonempty_queue_with_bounds;
+mod manual_nonempty_queue_with_lt_bounds;
 mod manual_queue;
 mod manual_queue_with_bounds;
 mod manual_queue_with_lt_bounds;
