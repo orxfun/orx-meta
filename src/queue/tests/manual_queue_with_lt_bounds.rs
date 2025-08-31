5504daa72106b7c935a501229324936cafa897b5
orxfun <orx.ugur.arikan@gmail.com>
2025-08-31 20:38:22 +0200

    manual builder implementations

--- src/queue/tests/manual_queue_with_lt_bounds.rs
+++ src/queue/tests/manual_queue_with_lt_bounds.rs
@@ -137,6 +137,40 @@ impl QueueComposition {
     }
 }
 
+// builder
+
+struct Builder<'i, Rem, Cur>(Cur, core::marker::PhantomData<&'i Rem>)
+where
+    Rem: Queue<'i>,
+    Cur: Queue<'i>;
+
+impl<'i, Rem> Builder<'i, Rem, EmptyQueue>
+where
+    Rem: Queue<'i>,
+{
+    pub fn new() -> Self {
+        Self(EmptyQueue, core::marker::PhantomData)
+    }
+}
+
+impl<'i, Rem, Cur> Builder<'i, Rem, Cur>
+where
+    Rem: Queue<'i>,
+    Cur: Queue<'i>,
+{
+    fn push_back(self, x: Rem::Front) -> Builder<'i, Rem::Back, Cur::PushBack<Rem::Front>> {
+        let current = self.0.push_back(x);
+        Builder(current, core::marker::PhantomData)
+    }
+
+    fn finish(self) -> Cur
+    where
+        Rem: Queue<'i, Front = Never>,
+    {
+        self.0
+    }
+}
+
 // tests
 
 impl<'i> Sth<'i> for char {}
@@ -254,6 +288,44 @@ fn compose_four() {
     let x = C::compose(x, 32);
     let x = C::compose(x, String::from("xyz"));
     let x = C::compose(x, true);
+
+    assert_eq!(x.front(), &'x');
+    let (f, x) = x.pop_front();
+    assert_eq!(f, 'x');
+
+    assert_eq!(x.front(), &32);
+    let (f, x) = x.pop_front();
+    assert_eq!(f, 32);
+
+    assert_eq!(x.front(), &String::from("xyz"));
+    let (f, x) = x.pop_front();
+    assert_eq!(f, String::from("xyz"));
+
+    assert_eq!(x.front(), &true);
+    let (f, x) = x.pop_front();
+    assert_eq!(f, true);
+
+    assert!(x.is_empty());
+}
+
+#[test]
+fn builder() {
+    type Target<'i> = Pair<'i, char, Pair<'i, i32, Pair<'i, String, Single<'i, bool>>>>;
+
+    let builder = Builder::<Target, _>::new();
+
+    let builder: Builder<Pair<i32, Pair<String, Single<bool>>>, Single<char>> =
+        builder.push_back('x');
+
+    let builder: Builder<Pair<String, Single<bool>>, Pair<char, Single<i32>>> =
+        builder.push_back(32);
+
+    let builder: Builder<Single<bool>, Pair<char, Pair<i32, Single<String>>>> =
+        builder.push_back("xyz".to_string());
+
+    let builder: Builder<EmptyQueue, Target> = builder.push_back(true);
+
+    let x = builder.finish();
 
     assert_eq!(x.front(), &'x');
     let (f, x) = x.pop_front();
