91247c374b4991f94667b03e1766e65e5442abfb
orxfun <orx.ugur.arikan@gmail.com>
2025-10-13 21:51:58 +0200

    impl queue methods

--- src/queue/non_empty.rs
+++ src/queue/non_empty.rs
@@ -36,7 +36,35 @@ where
         Self { front, back }
     }
 
+    // ref
+
+    pub fn front(&self) -> &F {
+        &self.front
+    }
+
+    pub fn back(&self) -> &B {
+        &self.back
+    }
+
+    // mut
+
+    pub fn front_mut(&mut self) -> &mut F {
+        &mut self.front
+    }
+
+    pub fn back_mut(&mut self) -> &mut B {
+        &mut self.back
+    }
+
     // into
+
+    pub fn into_front(self) -> F {
+        self.front
+    }
+
+    pub fn into_back(self) -> B {
+        self.back
+    }
 
     pub fn pop(self) -> (F, B) {
         (self.front, self.back)
