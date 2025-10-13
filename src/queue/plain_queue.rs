77f22793fdfdcc3c6dde838fdd681faae97f2e79
orxfun <orx.ugur.arikan@gmail.com>
2025-10-13 11:02:17 +0200

    doc and test queue_of

--- src/queue/plain_queue.rs
+++ src/queue/plain_queue.rs
@@ -1662,6 +1662,52 @@ impl<X1, X2, X3, X4, X5, X6, X7, X8> From<(X1, X2, X3, X4, X5, X6, X7, X8)>
 
 // queue of
 
+/// Note that queues of any sequence of types can be represented with three [`Queue`] implementations:
+/// * `Empty`: empty queue
+/// * `Single<F>`: a queue of a single element of type `F`
+/// * `Multi<F, B>`: a queue of multiple elements where the front element is of type `F`, and remaining elements form a queue of type `B`.
+///
+/// Notice the recursive definition enabled with the back of the `Multi` queue.
+///
+/// With these types, we can represent queues of elements
+/// * of type `A` with `Single<A>`
+/// * of types `A` and `B` with `Multi<A, Single<B>>`
+/// * of types `A`, `B` and `C` with `Multi<A, Multi<B, Single<C>>>`
+/// * of types `A`, `B`, `C` and `D` with `Multi<A, Multi<B, Multi<C, Single<S>>>>`
+/// * ...
+///
+/// However, hand-writing recursive type definitions of larger queues get more and more complex.
+///
+/// `queue_of` macro allows to generate these type definitions with a flat macro call.
+///
+/// Also see [`QueueBuilder`] which is often used together with `queue_of` macro to build complex types.
+///
+/// # Examples
+///
+/// ```
+/// use orx_meta::queue::*;
+/// use orx_meta::queue_of;
+///
+/// // 0
+/// let q: Empty = Empty::new();
+/// let q: queue_of!() = Empty::new();
+///
+/// // 1
+/// let q: Single<u32> = Empty::new().push(42);
+/// let q: queue_of!(u32) = Empty::new().push(42);
+///
+/// // 2
+/// let q: Multi<u32, Single<bool>> = Empty::new().push(42).push(true);
+/// let q: queue_of!(u32, bool) = Empty::new().push(42).push(true);
+///
+/// // 3
+/// let q: Multi<u32, Multi<bool, Single<char>>> = Empty::new().push(42).push(true).push('x');
+/// let q: queue_of!(u32, bool, char) = Empty::new().push(42).push(true).push('x');
+///
+/// // 4
+/// let q: Multi<u32, Multi<bool, Multi<char, Single<&'static str>>>> = Empty::new().push(42).push(true).push('x').push("foo");
+/// let q: queue_of!(u32, bool, char, &'static str) = Empty::new().push(42).push(true).push('x').push("foo");
+/// ```
 #[macro_export]
 macro_rules! queue_of {
     () => {
