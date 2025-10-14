b73f09ed0086a53c7129060d04d191fa542d3596
orxfun <orx.ugur.arikan@gmail.com>
2025-10-14 08:02:04 +0200

    tuple transformations

--- src/queue/non_empty.rs
+++ src/queue/non_empty.rs
@@ -119,3 +119,193 @@ impl<X1, X2, X3> Queue<X1, Queue<X2, S<X3>>> {
         )
     }
 }
+
+impl<X1, X2, X3, X4> Queue<X1, Queue<X2, Queue<X3, S<X4>>>> {
+    pub fn into_tuple(self) -> (X1, X2, X3, X4) {
+        (
+            self.front,
+            self.back.front,
+            self.back.back.front,
+            self.back.back.back.front,
+        )
+    }
+    pub fn as_tuple(&self) -> (&X1, &X2, &X3, &X4) {
+        (
+            &self.front,
+            &self.back.front,
+            &self.back.back.front,
+            &self.back.back.back.front,
+        )
+    }
+    pub fn as_tuple_mut(&mut self) -> (&mut X1, &mut X2, &mut X3, &mut X4) {
+        (
+            &mut self.front,
+            &mut self.back.front,
+            &mut self.back.back.front,
+            &mut self.back.back.back.front,
+        )
+    }
+}
+
+impl<X1, X2, X3, X4, X5> Queue<X1, Queue<X2, Queue<X3, Queue<X4, S<X5>>>>> {
+    pub fn into_tuple(self) -> (X1, X2, X3, X4, X5) {
+        (
+            self.front,
+            self.back.front,
+            self.back.back.front,
+            self.back.back.back.front,
+            self.back.back.back.back.front,
+        )
+    }
+    pub fn as_tuple(&self) -> (&X1, &X2, &X3, &X4, &X5) {
+        (
+            &self.front,
+            &self.back.front,
+            &self.back.back.front,
+            &self.back.back.back.front,
+            &self.back.back.back.back.front,
+        )
+    }
+    pub fn as_tuple_mut(&mut self) -> (&mut X1, &mut X2, &mut X3, &mut X4, &mut X5) {
+        (
+            &mut self.front,
+            &mut self.back.front,
+            &mut self.back.back.front,
+            &mut self.back.back.back.front,
+            &mut self.back.back.back.back.front,
+        )
+    }
+}
+
+impl<X1, X2, X3, X4, X5, X6> Queue<X1, Queue<X2, Queue<X3, Queue<X4, Queue<X5, S<X6>>>>>> {
+    pub fn into_tuple(self) -> (X1, X2, X3, X4, X5, X6) {
+        (
+            self.front,
+            self.back.front,
+            self.back.back.front,
+            self.back.back.back.front,
+            self.back.back.back.back.front,
+            self.back.back.back.back.back.front,
+        )
+    }
+    pub fn as_tuple(&self) -> (&X1, &X2, &X3, &X4, &X5, &X6) {
+        (
+            &self.front,
+            &self.back.front,
+            &self.back.back.front,
+            &self.back.back.back.front,
+            &self.back.back.back.back.front,
+            &self.back.back.back.back.back.front,
+        )
+    }
+    pub fn as_tuple_mut(&mut self) -> (&mut X1, &mut X2, &mut X3, &mut X4, &mut X5, &mut X6) {
+        (
+            &mut self.front,
+            &mut self.back.front,
+            &mut self.back.back.front,
+            &mut self.back.back.back.front,
+            &mut self.back.back.back.back.front,
+            &mut self.back.back.back.back.back.front,
+        )
+    }
+}
+
+impl<X1, X2, X3, X4, X5, X6, X7>
+    Queue<X1, Queue<X2, Queue<X3, Queue<X4, Queue<X5, Queue<X6, S<X7>>>>>>>
+{
+    pub fn into_tuple(self) -> (X1, X2, X3, X4, X5, X6, X7) {
+        (
+            self.front,
+            self.back.front,
+            self.back.back.front,
+            self.back.back.back.front,
+            self.back.back.back.back.front,
+            self.back.back.back.back.back.front,
+            self.back.back.back.back.back.back.front,
+        )
+    }
+    pub fn as_tuple(&self) -> (&X1, &X2, &X3, &X4, &X5, &X6, &X7) {
+        (
+            &self.front,
+            &self.back.front,
+            &self.back.back.front,
+            &self.back.back.back.front,
+            &self.back.back.back.back.front,
+            &self.back.back.back.back.back.front,
+            &self.back.back.back.back.back.back.front,
+        )
+    }
+    pub fn as_tuple_mut(
+        &mut self,
+    ) -> (
+        &mut X1,
+        &mut X2,
+        &mut X3,
+        &mut X4,
+        &mut X5,
+        &mut X6,
+        &mut X7,
+    ) {
+        (
+            &mut self.front,
+            &mut self.back.front,
+            &mut self.back.back.front,
+            &mut self.back.back.back.front,
+            &mut self.back.back.back.back.front,
+            &mut self.back.back.back.back.back.front,
+            &mut self.back.back.back.back.back.back.front,
+        )
+    }
+}
+
+impl<X1, X2, X3, X4, X5, X6, X7, X8>
+    Queue<X1, Queue<X2, Queue<X3, Queue<X4, Queue<X5, Queue<X6, Queue<X7, S<X8>>>>>>>>
+{
+    pub fn into_tuple(self) -> (X1, X2, X3, X4, X5, X6, X7, X8) {
+        (
+            self.front,
+            self.back.front,
+            self.back.back.front,
+            self.back.back.back.front,
+            self.back.back.back.back.front,
+            self.back.back.back.back.back.front,
+            self.back.back.back.back.back.back.front,
+            self.back.back.back.back.back.back.back.front,
+        )
+    }
+    pub fn as_tuple(&self) -> (&X1, &X2, &X3, &X4, &X5, &X6, &X7, &X8) {
+        (
+            &self.front,
+            &self.back.front,
+            &self.back.back.front,
+            &self.back.back.back.front,
+            &self.back.back.back.back.front,
+            &self.back.back.back.back.back.front,
+            &self.back.back.back.back.back.back.front,
+            &self.back.back.back.back.back.back.back.front,
+        )
+    }
+    pub fn as_tuple_mut(
+        &mut self,
+    ) -> (
+        &mut X1,
+        &mut X2,
+        &mut X3,
+        &mut X4,
+        &mut X5,
+        &mut X6,
+        &mut X7,
+        &mut X8,
+    ) {
+        (
+            &mut self.front,
+            &mut self.back.front,
+            &mut self.back.back.front,
+            &mut self.back.back.back.front,
+            &mut self.back.back.back.back.front,
+            &mut self.back.back.back.back.back.front,
+            &mut self.back.back.back.back.back.back.front,
+            &mut self.back.back.back.back.back.back.back.front,
+        )
+    }
+}
