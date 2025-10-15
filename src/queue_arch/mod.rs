98634ace9e27eba2a10bbcc4f25234e8f01f5862
orxfun <orx.ugur.arikan@gmail.com>
2025-10-15 13:31:55 +0200

    clean up

--- src/queue_arch/mod.rs
+++
@@ -1,9 +0,0 @@
-#[cfg(test)]
-mod tests;
-
-mod impl_queue;
-mod impl_queue_builder;
-mod impl_queue_composition;
-mod impl_queue_core;
-mod impl_queue_of;
-mod impl_queue_tuple_transformation;
--- src/queue_arch2/tests/mod.rs
+++
@@ -1,6 +0,0 @@
-mod builder;
-mod combined;
-mod combined_variants;
-mod core;
-mod queue_of;
-mod tuple;
