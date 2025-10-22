5b1ec56f17fa545d6ad30eab3fae31f7b22f2258
orxfun <orx.ugur.arikan@gmail.com>
2025-10-22 10:10:25 +0200

    document QueueSingle

--- src/queue/single.rs
+++ src/queue/single.rs
@@ -1,8 +1,12 @@
 use crate::queue::{Queue, StQueue};
 
+/// A statically-typed queue containing exactly one element of type `Front`.
+///
+/// See also the other [`StQueue`] implementation [`Queue`] which can be
+/// created by pushing a second element to this queue.
 #[derive(Clone, Copy, PartialEq, Eq, Debug, Default)]
-pub struct QueueSingle<F> {
-    pub(super) front: F,
+pub struct QueueSingle<Front> {
+    pub(super) front: Front,
 }
 
 impl<F> StQueue for QueueSingle<F> {
@@ -36,11 +40,47 @@ impl<F> StQueue for QueueSingle<F> {
 }
 
 impl<F> QueueSingle<F> {
+    /// Creates a new statically-typed queue [`StQueue`] containing exactly one `element`.
+    ///
+    /// Alternatively, we can use multiple element queue's [`new`]. This is for convenience to
+    /// allows to work with a single queue type while coding.
+    ///
+    /// [`new`]: crate::queue::Queue::new
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use orx_meta::queue::*;
+    ///
+    /// let queue: QueueSingle<u32> = QueueSingle::new(42);
+    /// assert_eq!(queue.len(), 1);
+    /// assert_eq!(queue.front(), &42);
+    ///
+    /// // alternatively, we can use `Queue::new`:
+    /// let queue: QueueSingle<u32> = Queue::new(42);
+    /// assert_eq!(queue.len(), 1);
+    /// assert_eq!(queue.front(), &42);
+    /// ```
     #[inline(always)]
     pub fn new(element: F) -> Self {
         Self { front: element }
     }
 
+    /// Pops and returns the element in the front of this queue.
+    ///
+    /// Since this element contains only one element, there is no remaining queue once the
+    /// front is popped. Therefore, the return type is only the element rather than a tuple.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use orx_meta::queue::*;
+    ///
+    /// let queue: QueueSingle<u32> = QueueSingle::new(42);
+    ///
+    /// let num = queue.pop();
+    /// assert_eq!(num, 42);
+    /// ```
     #[inline(always)]
     pub fn pop(self) -> F {
         self.front
